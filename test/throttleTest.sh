f0bb314080bd47292337510efa6fd557b5f93a6b
CharanMann <charan.mann@forgerock.com>
2017-10-12 16:34:55 -0400

    Throttle test

--- test/throttleTest.sh
+++ test/throttleTest.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 SECONDS=0
-VAR_tic=$(($(date +%s%N)/1000000))
-for i in $(seq 1 1000); do
-        VAR_CurlReq_Status="$(curl -s -I -L -H 'action: throttle' http://ig5.example.com:9000/history/emp1 | grep ^HTTP)"
+for i in $(seq 1 10000); do
+        VAR_CurlReq_Status="$(curl -s -I -H 'action: throttle' http://ig5.example.com:9000/history/emp1 | grep ^HTTP)"
         DURATION=$SECONDS
-        echo -e "\t [$((DURATION % 60))s] $VAR_CurlReq_Status"
+        echo -e "\t [$((DURATION))s] $VAR_CurlReq_Status"
 done
